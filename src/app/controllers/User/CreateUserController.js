00308dd24a40cee56e76145fb82aa7222216c3f7
Naiguel Santos <naiguelsantos@MacBook-Pro.local>
2020-07-07 21:59:47 -0300

    Add CreateUserService dependency

--- src/app/controllers/User/CreateUserController.js
+++ src/app/controllers/User/CreateUserController.js
@@ -1,13 +1,20 @@
 const createUserService = require('../../services/User/CreateUserService');
 
 class CreateUserController {
-    constructor() {
-        this.createUserService = createUserService;
+    constructor({ createUserUseCase }) {
+        this.createUserUseCase = createUserUseCase;
     }
 
     async handler(req, res, next) {
-        await this.createUserService.execute(req);
+        try {
+            const result = await this.createUserUseCase.execute(req);
+            res.status(200).json(result);
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
-module.exports = new CreateUserController();
+module.exports = new CreateUserController({
+    createUserUseCase: createUserService,
+});

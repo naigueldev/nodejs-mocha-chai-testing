48db7680b2911ff2040623a9c48dcbacfb96945e
Naiguel Santos <naiguelsantos@MacBook-Pro.local>
2020-07-08 21:08:58 -0300

    Add unique: true to attributes; Remove versionKey

--- src/app/models/UserModel.js
+++ src/app/models/UserModel.js
@@ -2,19 +2,24 @@ const mongoose = require('../database/mongodb');
 
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 100,
+const UserSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            min: 6,
+            max: 100,
+        },
+        email: {
+            type: String,
+            unique: true,
+            min: 6,
+            max: 100,
+        },
     },
-    email: {
-        type: String,
-        min: 6,
-        max: 100,
-    },
-});
+    { versionKey: false },
+);
 
 const UserModel = mongoose.model('User', UserSchema);
 

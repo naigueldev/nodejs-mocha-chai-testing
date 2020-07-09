9a571c52aa86f82a9abfc5014a92557fdb30f95c
Naiguel Santos <naiguelsantos@MacBook-Pro.local>
2020-07-08 22:45:54 -0300

    Load classes by container dependency injection

---
+++ src/loadModules.js
@@ -0,0 +1,31 @@
+const {
+    asClass, asValue, createContainer, Lifetime, listModules,
+} = require('awilix');
+
+const container = createContainer();
+const cwd = `${process.cwd()}/src/app/`;
+container.loadModules([
+    'controllers/**/*.js',
+    'repositories/*.js',
+    'services/**/*.js',
+], {
+    cwd,
+    formatName: 'camelCase',
+    resolverOptions: {
+        lifetime: Lifetime.SINGLETON,
+        register: asClass,
+    },
+});
+
+container.loadModules([
+    'models/*.js',
+], {
+    cwd,
+    formatName: 'camelCase',
+    resolverOptions: {
+        lifetime: Lifetime.SINGLETON,
+        register: asValue,
+    },
+});
+
+module.exports = container;

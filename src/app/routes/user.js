b244880b30e1306536cbcdab0a015a7da4d593ad
Naiguel Santos <naiguelsantos@MacBook-Pro.local>
2020-07-08 22:44:37 -0300

    Get controller from container

--- src/app/routes/user.js
+++ src/app/routes/user.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { CreateUserController } = require('../controllers/User');
+const { createUserController } = require('../../loadModules').cradle;
 
 const routes = Router();
 
@@ -7,6 +7,6 @@ routes.get('/', (req, res) => {
     res.status(200).json({ message: 'Ok' });
 });
 
-routes.post('/', CreateUserController.handler.bind(CreateUserController));
+routes.post('/', createUserController.handler.bind(createUserController));
 
 module.exports = routes;
